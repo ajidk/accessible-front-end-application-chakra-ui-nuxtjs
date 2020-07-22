58f4e7c87594bb746c3489eb957468d4fd8f0c32
surajidk <surajidk12@gmail.com>
2020-07-22 10:22:14 +0700

    make darkmode

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="container">
     <c-theme-provider>
-      <c-color-mode-provider>
-        <c-box as="section">
+      <c-color-mode-provider #default="{ colorMode }">
+        <c-box
+          v-bind="mainStyles[colorMode]"
+            w="98.85vw"
+          as="section"
+        >
           <c-reset />
+          <nav-bar />
           <nuxt />
         </c-box>
       </c-color-mode-provider>
@@ -13,14 +18,29 @@
 
 <script>
 import { CThemeProvider, CColorModeProvider, CReset, CBox } from '@chakra-ui/vue'
-
+import NavBar from '@/components/NavBar'  
 export default {
   name: 'DefaultLayout',
   components: {
     CThemeProvider,
     CColorModeProvider,
     CReset,
-    CBox
+    CBox,
+    NavBar
+  },
+  data () {
+    return {
+      mainStyles: {
+        dark: {
+          bg: 'gray.900',
+          color: 'whiteAlpha.900'
+        },
+        light: {
+          bg: 'whiteAlpha.900',
+          color: 'gray.900'
+        }
+      }
+    }
   }
 }
 </script>

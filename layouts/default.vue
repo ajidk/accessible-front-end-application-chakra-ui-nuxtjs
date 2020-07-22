e98502717f7e238cffb825df63d331dbc5dff945
surajidk <surajidk12@gmail.com>
2020-07-22 09:44:54 +0700

    install package cakra ui and nuxtjs/emoticon

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <c-theme-provider>
+      <c-color-mode-provider>
+        <c-box as="section">
+          <c-reset />
+          <nuxt />
+        </c-box>
+      </c-color-mode-provider>
+    </c-theme-provider>
+  </div>
+</template>
+
+<script>
+import { CThemeProvider, CColorModeProvider, CReset, CBox } from '@chakra-ui/vue'
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    CThemeProvider,
+    CColorModeProvider,
+    CReset,
+    CBox
+  }
+}
+</script>

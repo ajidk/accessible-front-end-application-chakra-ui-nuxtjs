2eb72d5487e570636c2a8fa8615f7be70d507266
surajidk <surajidk12@gmail.com>
2020-07-22 13:12:38 +0700

    finishing ui cakra

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,53 @@
+// pages/index.vue
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        chakra-ui-explorer
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <c-box as="main" d="flex" align-items="space-between" flex-direction="column" w="auto" p="16">
+    <c-input v-model="search" placeholder="Search components..." size="lg" mb="10" is-full-width />
+    <c-grid template-columns="repeat(4, 1fr)" gap="3" p="5">
+      <c-box v-for="(chakraComponent, index) of filteredComponents" :key="index" h="10">
+        {{ chakraComponent.name }}
+        <c-badge>
+          <c-link
+            is-external
+            :href="lowercase(`https://vue.chakra-ui.com/${chakraComponent.name}`)"
+          >
+            <c-icon name="info" size="18px" />
+          </c-link>
+        </c-badge>
+      </c-box>
+    </c-grid>
+  </c-box>
 </template>
 
 <script>
-export default {}
+import { CBox, CInput, CGrid, CLink, CBadge, CIcon } from "@chakra-ui/vue";
+import { components as chakraComponents } from "../data";
+export default {
+  components: {
+    CBox,
+    CInput,
+    CGrid,
+    CBadge,
+    CIcon,
+    CLink
+  },
+  data() {
+    return {
+      search: ""
+    };
+  },
+  computed: {
+    filteredComponents() {
+      return chakraComponents.filter(component => {
+        return this.lowercase(component.name).includes(
+          this.lowercase(this.search)
+        );
+      });
+    }
+  },
+  methods: {
+    lowercase(value) {
+      return value.toLowerCase();
+    }
+  }
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
